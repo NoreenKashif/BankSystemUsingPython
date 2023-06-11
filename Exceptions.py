6cbfb568e62cabc2b054f756fbcc01883ad9225b
NoreenKashif <89309542+NoreenKashif@users.noreply.github.com>
2023-06-12 03:22:35 +0500

    Add files via upload

---
+++ Exceptions.py
@@ -0,0 +1,19 @@
+class PasswordNotMatch(Exception):
+    pass
+
+
+class InvalidPassword(Exception):
+    pass
+
+
+class DbErrors(Exception):
+    pass
+
+
+class UserNotFound(Exception):
+    pass
+
+
+class WrongPassword(Exception):
+    pass
+
